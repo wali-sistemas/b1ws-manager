b14ffe87c7ef4a7fc7175329e543db7b1c9bb162
jguisaor <jguisaorestrepo@gmail.com>
2019-03-04 21:32:43 -0500

    Subiendo ajustes

---
+++ src/main/java/co/manager/ejb/StateManager.java
@@ -0,0 +1,53 @@
+package co.manager.ejb;
+
+import co.manager.b1ws.state.*;
+import co.manager.util.Constants;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author jguisao
+ */
+@Stateless
+public class StateManager {
+    private static final Logger CONSOLE = Logger.getLogger(StateManager.class.getSimpleName());
+    private StatesService service;
+
+    @Inject
+    private ManagerApplicationBean appBean;
+
+    @PostConstruct
+    private void initialize() {
+        try {
+            service = new StatesService(
+                    new URL(String.format("https://74601289-a97b-43a2-8c89-7a5ac1ed92d7.mock.pstmn.io/B1WS/WebReferences/StatesService.wsdl"
+                            /*appBean.obtenerValorPropiedad(Constants.B1WS_WSDL_URL), Constants.B1WS_STATE_SERVICE*/)));
+        } catch (MalformedURLException e) {
+            CONSOLE.log(Level.SEVERE, "No fue posible iniciar la instancia de StateService. ", e);
+        }
+    }
+
+    public Response getState(String sessionId) {
+        MsgHeader header = new MsgHeader();
+        header.setSessionID(sessionId);
+        header.setServiceName(Constants.B1WS_STATE_SERVICE);
+
+        StateParams docParams = new StateParams();
+        docParams.setCode("100");
+        docParams.setCountry("BR");
+        docParams.setName("EXTERIOR");
+
+        GetState params = new GetState();
+        params.setStateParams(docParams);
+
+        GetStateResponse response = service.getStatesServiceSoap12().getState(params, header);
+        return Response.ok(response).build();
+    }
+}
