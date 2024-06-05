f3f3e032990c812523edd9dc47a44e86915ba216
wali <sistemasigb@gmail.com>
2024-06-05 11:52:42 -0500

    cambio a master

--- src/main/java/co/manager/persistence/facade/SalesPersonSAPFacade.java
+++ src/main/java/co/manager/persistence/facade/SalesPersonSAPFacade.java
@@ -322,7 +322,7 @@ public class SalesPersonSAPFacade {
         StringBuilder sb = new StringBuilder();
         sb.append("select cast(ifnull(\"Telephone\",'01') as varchar(3))as whsCodeDefTire,'01' as WhsCodeDefLub/*,'32' as WhsCodeDefPart*/ ");
         if (companyName.contains("VARROC")) {
-            sb.append(",cast(ifnull(\"Memo\",'26') as varchar(3)) as whsCodeDefTireCali ");
+            sb.append("'32' as WhsCodeDefPart,cast(ifnull(\"Memo\",'26') as varchar(3)) as whsCodeDefTireCali ");
         } else {
             sb.append(",'00' as whsCodeDefTireCali ");
         }
